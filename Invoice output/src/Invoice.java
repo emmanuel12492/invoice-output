507d435c19cc73722d054d52b710ddf4c9d0e0e2
koech emmanuel <161034393+emmanuel12492@users.noreply.github.com>
2024-10-03 17:26:19 +0300

    Add files via upload

---
+++ Invoice output/src/Invoice.java
@@ -0,0 +1,30 @@
+class main {
+    public static void main(String[] args) {
+        System.out.println("*******************C026-01-0752/2023*******************");
+        System.out.println("#99 ACS Building Gilmore Ave. New Mla.. Quezon City  *  Tel. No:123-4567");
+        System.out.println("VAT REG. TIN 098-765-4321-0000");
+        System.out.println("SALES INVOICE");
+        System.out.println();
+        System.out.println("Sold to:Emmanuel kipkorir koech");
+        System.out.println("Address:private bag");
+        System.out.println("Date:28/04/2020            Contact Number:0100100100");
+        System.out.println();
+        System.out.println("===========================================================");
+        System.out.println("Qty | Item Description  | Unit Price  | Amount");
+        System.out.println("===========================================================");
+        System.out.println("4   | Pencil            | 20          | 80.00");
+        System.out.println("-----------------------------------------------------------");
+        System.out.println("10  | Duster            | 50          | 500.00");
+        System.out.println("-----------------------------------------------------------");
+        System.out.println("9   | Pens              | 30          | 270.00");
+        System.out.println("-----------------------------------------------------------");
+        System.out.println("10  | crayon            | 80          | 800.00");
+        System.out.println("===========================================================");
+        System.out.println("                          | Sub Total  | 1650.00");
+        System.out.println("===========================================================");
+        System.out.println("                          | VAT(16%)   | 264.00");
+        System.out.println("===========================================================");
+        System.out.println("                          | Total Amount | 1914.00");
+        System.out.println("===========================================================");
+    }
+}
